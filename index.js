948fa2a4184c4cdf3cd53910014eb93a4a785cd2
Brett Byrd <byrdb2@yahoo.com>
2023-02-28 19:30:47 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+// Code your solutions in this f
+function writeCards(names, event){
+console.log(names)
+console.log(event)
+const messages = []
+for (let i = 0; i < names.length; i++){
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+}
+return messages
+}
+
+function countDown(n){
+    
+
+while (n >= 0) {
+console.log(n--)
+}
+}
